8e52b1cf34c88650945df023cf0b8fbf8589b0bc
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2023-05-20 12:24:23 +0300

    Add files via upload

--- discirandevu.sql
+++ discirandevu.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Anamakine: sql312.epizy.com
--- Üretim Zamanı: 20 May 2023, 03:45:02
--- Sunucu sürümü: 10.4.17-MariaDB
--- PHP Sürümü: 7.2.22
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 20 May 2023, 11:23:36
+-- Sunucu sürümü: 10.4.27-MariaDB
+-- PHP Sürümü: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Veritabanı: `epiz_34242564_discirandevu`
+-- Veritabanı: `discirandevu`
 --
 
 -- --------------------------------------------------------
@@ -30,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `doktorlar` (
   `doktor_id` smallint(5) UNSIGNED NOT NULL,
-  `doktor_kullaniciadi` varchar(60) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `doktor_adsoyadi` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `sifre` varchar(128) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `unvan` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+  `doktor_kullaniciadi` varchar(60) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `doktor_adsoyadi` varchar(100) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `sifre` varchar(128) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `unvan` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Tablo döküm verisi `doktorlar`
@@ -53,23 +52,25 @@ INSERT INTO `doktorlar` (`doktor_id`, `doktor_kullaniciadi`, `doktor_adsoyadi`, 
 
 CREATE TABLE `hastalar` (
   `hasta_id` smallint(5) UNSIGNED NOT NULL,
-  `adsoyadi` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `email` varchar(120) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `telno` varchar(11) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `adsoyadi` varchar(100) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `telno` varchar(11) NOT NULL,
   `doktor_id` int(11) NOT NULL,
   `randevu_tarih` date NOT NULL,
   `randevu_saat` time NOT NULL,
-  `hasta_text` varchar(200) COLLATE utf8mb4_turkish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+  `hasta_text` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Tablo döküm verisi `hastalar`
 --
 
 INSERT INTO `hastalar` (`hasta_id`, `adsoyadi`, `email`, `telno`, `doktor_id`, `randevu_tarih`, `randevu_saat`, `hasta_text`) VALUES
-(5, 'Ayşe', 'asddas@dasdas.com', '25465452', 2, '2023-05-19', '02:11:00', '\0Diş bakımı'),
-(12, 'Mehmet Güven', 'mehmet@hotmail.com', '05484564545', 1, '2023-05-24', '09:00:00', 'Diş ağrısı NOT alinidi'),
-(32, 'adsdas', 'asdsad@adasads.com', '05467894561', 1, '2023-05-21', '10:19:00', 'Ağrı');
+(5, 'Ayşe', 'asddas@dasdas.com', '25465452', 2, '2023-05-19', '02:11:00', 'Diş bakımı'),
+(12, 'Mehmet Güven', 'mehmet@hotmail.com', '05484564564', 1, '2023-05-21', '09:00:00', 'Diş ağrısı NOT alinidi'),
+(13, 'Mehmet Yüksek', 'mehmet@gmail.com', '05461234567', 1, '2023-05-19', '13:59:00', 'Diş ağrısı'),
+(30, 'Ayşe Öztürk', 'ayse@gmail.com', '05891234567', 1, '2023-05-20', '18:20:00', 'Diş teli'),
+(31, 'Kadriye Türk', 'kadriye@hotmail.com', '05784567891', 1, '2023-05-20', '14:50:00', 'Diş ağrısı');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -101,7 +102,7 @@ ALTER TABLE `doktorlar`
 -- Tablo için AUTO_INCREMENT değeri `hastalar`
 --
 ALTER TABLE `hastalar`
-  MODIFY `hasta_id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `hasta_id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
